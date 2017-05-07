8cf754134abdda39f0b905fa589e304ac711a505
zgwldrc <zgwldrc@163.com>
2017-05-07 15:37:34 +0800

    add poll models and register it in admin site

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'NAME': 'app',
     }
 }
 
@@ -107,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'PRC'
 
 USE_I18N = True
 
--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField('问题', max_length=200)
+    pub_date = models.DateTimeField('发布日期')
+
+    def __str__(self):
+        return self.question_text
+
+    def is_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    choice_text = models.CharField('选择', max_length=200)
+    votes = models.IntegerField('票数', default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+
