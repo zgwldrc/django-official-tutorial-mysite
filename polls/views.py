b8efff7683461fc7d653bcd270693cd9c3a614cf
zgwldrc <zgwldrc@163.com>
2017-05-10 10:56:11 +0800

    1.增加多个测试类

--- polls/views.py
+++ polls/views.py
@@ -17,6 +17,12 @@ class IndexView(ListView):
 class QuestionDetailView(DetailView):
     model = Question
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
